369dd338d618d7e32e39aedb72ab24cbaf6db0ba
VAIBHAV <101047527+billigoat76@users.noreply.github.com>
2025-06-29 15:39:41 +0530

    episode-5-Let's-get-hooked

--- spicetalks/src/components/Body.jsx
+++ spicetalks/src/components/Body.jsx
@@ -3,8 +3,13 @@ import RestaurantCard from './RestaurantCard'; // Adjust the path as needed
 
 
 export default function Body() {
-
   
+  const[resList,setResList] = useState([]);
+  
+  function filterTopRatedRestaurants(){
+    const filteredlist = resList.filter((res) => res.avgRating >4);
+    setResList(filteredlist);
+  }
   return (
     <main className="bg-cream min-h-screen p-6">
       {/* Search Bar */}
@@ -19,12 +24,19 @@ export default function Body() {
         >
           Search
         </button>
+        <button
+          className="px-6 py-2 bg-orange-500 text-white rounded-lg hover:bg-orange-600 transition cursor-pointer"
+          onClick={filterTopRatedRestaurants}
+        >
+          Top Rated Restaurants
+        </button>
       </div>
 
       {/* Restaurant Cards Grid */}
       <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        <RestaurantCard/>
-        <RestaurantCard/>
+        {
+          resList.map((restaurant) => (<RestaurantCard key={restaurant.data.id} resData={restaurant}/>))
+        }
       </div>
     </main>
   );
--- spicetalks/src/components/RestaurantCard.jsx
+++ spicetalks/src/components/RestaurantCard.jsx
@@ -1,26 +1,28 @@
 import React from 'react';
 
-export default function RestaurantCard() {
+export default function RestaurantCard({resData}) {
+  const {resName,avgRating,deliveryTime,cuisines,costForTwo} = resData;
   return (
     <div className="bg-white rounded-xl shadow-lg hover:shadow-xl transition-all duration-200 overflow-hidden w-72">
       {/* Image */}
       <img
         // src={imageUrl}
-        // alt={name}
+        alt={resName}
         className="h-44 w-full object-cover"
       />
 
       {/* Content */}
       <div className="p-4">
-        <h3 className="text-lg font-bold text-gray-800 mb-1">{name}</h3>
+        <h3 className="text-lg font-bold text-gray-800 mb-1">{resName}</h3>
+        <h5 className="text-lg font-bold text-gray-800 mb-1">{cuisines}</h5>
         <div className="flex items-center text-sm text-gray-600 mb-2">
           <span className="bg-green-500 text-white px-2 py-0.5 rounded mr-2 font-semibold">
-            ⭐ 4
+            ⭐ {avgRating}
           </span>
-          • <span className="ml-2">28 mins</span>
+          • <span className="ml-2">{deliveryTime}</span>
         </div>
         <p className="text-sm text-gray-500 truncate">
-          
+            {costForTwo}
         </p>
       </div>
     </div>
