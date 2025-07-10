79947f10103d1f8d859f0f8f33b992a3256bb324
VAIBHAV <101047527+billigoat76@users.noreply.github.com>
2025-07-10 23:00:55 +0530

    "grouped-test-cases"

--- spicetalks/src/__tests__/ContactUs.test.jsx
+++ spicetalks/src/__tests__/ContactUs.test.jsx
@@ -1,29 +1,30 @@
-import React from 'react'; // Optional in modern setups but safe to include
 import { render, screen } from '@testing-library/react';
 import ContactUs from '../components/ContactUs'; // correct path to your component
 
-test('should load contact us component', () => {
+describe("Contact us page test cases",()=>{
+    it('should load contact us component', () => {
     render(<ContactUs />);
     const heading = screen.getByText("Send us a message");
     expect(heading).toBeInTheDocument();
 });
 
-test('should load button inside contactus component', () => {
+it('should load button inside contactus component', () => {
     render(<ContactUs />);
     const button = screen.getByRole("button");
     expect(button).toBeInTheDocument();
 });
 
-test('should load input email inside contactus component', () => {
-    render(<ContactUs/>);
+it('should load input email inside contactus component', () => {
+    render(<ContactUs/>);   
     const email = screen.getByPlaceholderText('you@example.com')
     expect(email).toBeInTheDocument();
 });
 
-test('should load two input boxes inside contact us component',()=>{
+it('should load three input boxes inside contact us component',()=>{
     render(<ContactUs/>)
 
     const inputBoxes = screen.getAllByRole('textbox');
 
     expect(inputBoxes.length).toBe(3)
+})
 })
